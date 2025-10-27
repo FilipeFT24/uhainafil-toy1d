00df3360272fb4ed9fec0f905789bcccc96ffb4d
Filipe Forte Tenreiro <filipe.tenreiro1@gmail.com>
2025-10-27 16:10:29 +0100

    WORKS FOR SOME CASES

--- main.m
+++ main.m
@@ -80,11 +80,10 @@ data      = setdata(test, 0);
 g         = MSH(data.xv, p);
 drytol    = 1e-02;
 velcutoff = drytol;
-%% 
 vellim    = 1;
 itype     = 1;     % 0: uhaina: interpolation
                    % 1: my:     projection
-CFL       = 0.05;  % CFL./(2.*p+1);
+CFL       = 0.04;  % CFL./(2.*p+1);
 penParam  = 1000;
 run       = 1;
 %--------------------------------------------------------------------------
--- wetdry/limN.m
+++ wetdry/limN.m
@@ -7,7 +7,6 @@ Kf     = K+1;
 fc     = g.bf;
 fl     = g.bfl;
 fr     = g.bfr;
-rtol   = eps;
 drytol = g.drytol;
 g.fix  = false(K, 1);
 g.zb   = g.zbinit;
@@ -40,24 +39,30 @@ H_r    = Z_r-Zbr;
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Loop through "problematic" cells:
 %--------------------------------------------------------------------------
-log1 = H_m(:, 1) < 0 | (H_l(:, 1) < drytol & H_r(:, 1) < drytol);
+log1 = H_m(:, 1) < drytol | (H_l(:, 1) < drytol & H_r(:, 1) < drytol);
 c    = find(log1);
 m    = size(c, 1);
 %--------------------------------------------------------------------------
 for i = 1:m
+    %----------------------------------------------------------------------
     o              = c    (i, 1);
     Zbo            = Zbdof(o, :);
-    H_dof(o, :)    = 0;
+    H_o            = 0.9.*drytol;
+    %----------------------------------------------------------------------
+    H_dof(o, :)    = H_o;
     Hudof(o, :)    = 0;
-    Z_dof(o, :)    = Zbo;
-    H_m  (o, 1)    = 0;
+    Z_dof(o, :)    = H_o+Zbo;
+    %----------------------------------------------------------------------
+    H_m  (o, 1)    = H_o;
     Hum  (o, 1)    = 0;
-    g.x  (o, :, 1) = Zbo;
-    g.x  (o, :, 2) = 0;
-    H_l  (o, 1)    = 0;
-    H_r  (o, 1)    = 0;
-    Z_l  (o, 1)    = Zbl(o, 1);
-    Z_r  (o, 1)    = Zbr(o, 1);
+    H_l  (o, 1)    = H_o;
+    H_r  (o, 1)    = H_o;
+    Z_l  (o, 1)    = Z_dof(o, :)*fl';
+    Z_r  (o, 1)    = Z_dof(o, :)*fr';
+    %----------------------------------------------------------------------
+    g.x  (o, :, 1) = Z_dof(o, :);
+    g.x  (o, :, 2) = Hudof(o, :);
+    g.zb (o, :)    = Z_dof(o, :);
     %----------------------------------------------------------------------
 end
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -81,17 +86,12 @@ f               = find(log2);
 n               = size(f, 1);
 %--------------------------------------------------------------------------
 
-H1    = g.x(:, :, 1)-g.zbinit-drytol;
+H1    = g.x(:, :, 1)-g.zb-drytol;
 Haux1 = [H1*fl', H1*fr'];
+
 flag = 0;
 if n > 2
-    stop = 1;
-    flag = 0;
-end
-
-if g.nit > 6500
-    flag = 0;
-    x = 1;
+    flag = 1;
 end
 
 %--------------------------------------------------------------------------
@@ -103,50 +103,35 @@ for i = 1:n
     R   = r+1;
     Z_L = Z_r(L, 1);
     Z_R = Z_l(R, 1);
-    H_L = H_r(L, 1);
-    H_R = H_l(r, 1);
     %----------------------------------------------------------------------
-    if log2l(r, 1) && (~log2r(l, 1) || log2r(l, 1) && H_R > H_L)
+    if Z_L <= Z_R
         %------------------------------------------------------------------
-        % WET/DRY
-        %------------------------------------------------------------------
-        Z_X            = min(Z_L, Z_R);
-        g.x  (l, :, 1) = Z_X;
+        g.x  (l, :, 1) = Z_L;
         g.x  (l, :, 2) = Hum(l, 1);
-        g.zb (l, :)    =-H_m(l, 1)+Z_X;
+        g.zb (l, :)    =-H_m(l, 1)+Z_L;
         g.fix(l, 1)    = true;
-        if (Z_L < Z_R && Z_l(r, 1) < Z_X) || (Z_L > Z_R && Z_l(r, 1) < Z_X) % up/down %%% ta aqui qq coisa mal mas as condiçoes parecem ser estas
-            g.x  (r, :, 1) = Z_X;
+        if (Z_r(l, 1) > Z_l(r, 1) && Z_l(r, 1) < Z_L) || (Z_r(l, 1) < Z_l(r, 1) && Z_l(r, 1) > Z_L) % run-up/down
+            g.x  (r, :, 1) = Z_L;
             g.x  (r, :, 2) = Hum(r, 1);
-            g.zb (r, :)    =-H_m(r, 1)+Z_X;
+            g.zb (r, :)    =-H_m(r, 1)+Z_L;
             g.fix(r, 1)    = true;
-
-            flag = 0;
         end
         %------------------------------------------------------------------
-        %flag = 0;
-        PLOT(flag, g, l, r, Z_dof, Zbdof, drytol);
     else
         %------------------------------------------------------------------
-        % DRY/WET
-        %------------------------------------------------------------------
-        Z_X            = min(Z_L, Z_R);
-        g.x  (r, :, 1) = Z_X;
+        g.x  (r, :, 1) = Z_R;
         g.x  (r, :, 2) = Hum(r, 1);
-        g.zb (r, :)    =-H_m(r, 1)+Z_X;
+        g.zb (r, :)    =-H_m(r, 1)+Z_R;
         g.fix(r, 1)    = true;
-        if (Z_R < Z_L && Z_r(l, 1) < Z_X) || (Z_R > Z_L && Z_r(l, 1) < Z_X) % up/down
-            g.x  (l, :, 1) = Z_X;
+        if (Z_l(r, 1) > Z_r(l, 1) && Z_r(l, 1) < Z_R) || (Z_l(r, 1) < Z_r(l, 1) && Z_r(l, 1) > Z_R) % run-up/down
+            g.x  (l, :, 1) = Z_R;
             g.x  (l, :, 2) = Hum(l, 1);
-            g.zb (l, :)    =-H_m(l, 1)+Z_X;
+            g.zb (l, :)    =-H_m(l, 1)+Z_R;
             g.fix(l, 1)    = true;
-
-            flag = 0;
         end
         %------------------------------------------------------------------
-        %flag = 0;
-        PLOT(flag, g, l, r, Z_dof, Zbdof, drytol);
     end
+    PLOT(flag, g, l, r, Z_dof, Zbdof, drytol);
     %----------------------------------------------------------------------
 end
 
@@ -191,13 +176,13 @@ if flag
     hold on;
     for j = l-1:r+1
         plot(g.xydc(j, aux), Z_dof(j, aux), '--ob');
-        plot(g.xydc(j, aux), Zbdof(j, aux)+drytol,  ':*k');
+        plot(g.xydc(j, aux), Zbdof(j, aux),  ':*k');
     end
     subplot(1, 2, 2);
     hold on;
     for j = l-1:r+1
         plot(g.xydc(j, aux), g.x (j, aux, 1), '--ob');
-        plot(g.xydc(j, aux), g.zb(j, aux)+drytol, ':*k');
+        plot(g.xydc(j, aux), g.zb(j, aux), ':*k');
     end
     close(f1);
 end
